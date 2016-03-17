3187f79a2fc43bdf81df5851ade7aade88fd6c7f
pabloascencao <pablo.ascencao@globant.com>
2016-03-17 17:52:23 -0300

    working on header

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -23,7 +23,34 @@
     /** @ngInject */
     function NavbarController() {
       var vm = this;
-
+      vm.obPre = {
+        name: 'OB Pre',
+        fullName: 'Olimpiadas Biblicas preadolescentes',
+        logoImg: '/assets/images/logoOB-small.png',
+        items: [
+          {name:'Vision',link: ''},
+          {name:'Como Participar?',link: ''},
+          {name:'Zonas Habilitadas',link: ''},
+          {name:'Trabajo Internacional',link: ''},
+          {name:'Suma tu zona',link: ''},
+          {name:'Descargas',link: ''}
+        ]
+      }
+      vm.obExt = {
+        name: 'OB Extemas',
+        fullName: 'Olimpiadas Biblicas Extremas',
+        logoImg: '/assets/images/logoObExt-small.jpg',
+        items: [
+          {name:'Vision',link: ''},
+          {name:'Como Participar?',link: ''},
+          {name:'Descargas',link: ''}
+        ]
+      }
+      vm.lgf = {name: "La Gran final", link:''}
+      vm.cart = {name: "Carrito", link:''}
+      vm.cpa = {name: "Vision de CPA", link:''}
+      vm.ma = {name: "Ministerios asociados", link:''}
+      vm.news = {name: "Novedades", link:''}
     }
   }
 
